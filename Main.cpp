cf1f84c79bfc98ece393d5b0c34bfa1e682755d4
BogdanCojocar <bogdan.cojocar@gmail.com>
2014-03-08 14:00:31 +0200

    Cleaned main.

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,6 @@
  *
  * Created on March 3, 2014, 10:29 PM
  */
-
-#include <cstdlib>
-
-using namespace std;
-
-/*
- * 
- */
 int main(int argc, char** argv) {
     return 0;
 }
--- build/Debug/Cygwin_4.x-Windows/Main_nomain.o.d
+++
@@ -1,1 +0,0 @@
-build/Debug/Cygwin_4.x-Windows/Main_nomain.o: Main.cpp
